efaef3addb8102ac2cc33860627a3cc57ac2a188
Ken Yates <pushlaka@yahoo.com>
2015-02-01 19:08:16 -0500

    Initial Build
    added code to repo

---
+++ LittleSomething/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace LittleSomething
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRows = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDecimals = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblRoundedAvg = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter Number of Rows:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 91);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(182, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rows:";
+            // 
+            // lblRows
+            // 
+            this.lblRows.AutoSize = true;
+            this.lblRows.Location = new System.Drawing.Point(317, 91);
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(44, 13);
+            this.lblRows.TabIndex = 5;
+            this.lblRows.Text = "lblRows";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sum:";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(317, 116);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(38, 13);
+            this.lblSum.TabIndex = 7;
+            this.lblSum.Text = "lblSum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Max Decimals:";
+            // 
+            // lblDecimals
+            // 
+            this.lblDecimals.AutoSize = true;
+            this.lblDecimals.Location = new System.Drawing.Point(317, 198);
+            this.lblDecimals.Name = "lblDecimals";
+            this.lblDecimals.Size = new System.Drawing.Size(60, 13);
+            this.lblDecimals.TabIndex = 9;
+            this.lblDecimals.Text = "lblDecimals";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Average:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(317, 143);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(57, 13);
+            this.lblAverage.TabIndex = 11;
+            this.lblAverage.Text = "lblAverage";
+            // 
+            // lblRoundedAvg
+            // 
+            this.lblRoundedAvg.AutoSize = true;
+            this.lblRoundedAvg.Location = new System.Drawing.Point(317, 171);
+            this.lblRoundedAvg.Name = "lblRoundedAvg";
+            this.lblRoundedAvg.Size = new System.Drawing.Size(80, 13);
+            this.lblRoundedAvg.TabIndex = 13;
+            this.lblRoundedAvg.Text = "lblRoundedAvg";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(221, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Rounded Avg:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 301);
+            this.Controls.Add(this.lblRoundedAvg);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblDecimals);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblRows);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRows;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDecimals;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblRoundedAvg;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ LittleSomething/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LittleSomething
+{
+    public partial class Form1 : Form
+    {
+        DataTable _dt; 
+        Random _ran;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _ran = new Random();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string errorMessage = string.Empty;
+            errorMessage = ValidForm();
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show(errorMessage);
+                this.textBox1.Focus();
+                return;
+            }
+            _dt = new DataTable("numRows");
+            _dt.Columns.Add("value");
+            // CreateRows
+            CreateRows();
+            this.dataGridView1.DataSource = _dt;
+
+            DoStats();
+        }
+        #region Private Methods
+
+        private void DoStats()
+        { 
+            // sum, average, decimal, rows
+            int rows = _dt.Rows.Count;
+            double sum = 0.0;
+            double avg = 0.0;
+            double roundedAvg = 0.0;
+            int intMaxDecPlaces = 0;
+            
+            foreach (DataRow dr in _dt.Rows)
+            {
+                double val = double.Parse(dr[0].ToString());
+                int intDecPlaces = GetNumberOfDecimals(val);
+                if (intDecPlaces > intMaxDecPlaces)
+                {
+                    intMaxDecPlaces = intDecPlaces;
+                }
+                sum += val;
+            }
+            avg = (sum / _dt.Rows.Count);
+            roundedAvg = Math.Round(avg, intMaxDecPlaces);
+
+            // Display status
+            this.lblAverage.Text = avg.ToString();
+            this.lblDecimals.Text = intMaxDecPlaces.ToString();
+            this.lblRows.Text = rows.ToString();
+            this.lblSum.Text = sum.ToString();
+            this.lblRoundedAvg.Text = roundedAvg.ToString();
+        }
+        
+        private int GetNumberOfDecimals(double val)
+        {
+            int returnValue = 0;
+            string strVal = val.ToString();
+            int dec = strVal.IndexOf(".");
+            string strDecVal = strVal.Substring(dec + 1);
+            returnValue = strDecVal.Length;
+            return returnValue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CreateRows()
+        {
+            int numRows = int.Parse(this.textBox1.Text.Trim());
+            for (int i = 1; i <= numRows; i++)
+            { 
+                // Calculate number
+                double val = CalculateNumber();
+
+                // Add row
+                DataRow dr = _dt.NewRow();
+                dr["value"] = val;
+                _dt.Rows.Add(dr);
+            }
+        }
+
+        private double CalculateNumber()
+        {
+            double returnValue = 0.0;
+            string decimalValue = string.Empty;
+            string totalValue = string.Empty;
+            int decimalPlaces = 0;
+             
+            // Determine decimal places
+            decimalPlaces = _ran.Next(0,9);
+            for (int i = 1; i < decimalPlaces; i++)
+            {
+                decimalValue += _ran.Next(0, 9).ToString();
+            }
+            totalValue = _ran.Next(1, 1000).ToString() + "." + decimalValue;
+            returnValue = double.Parse(totalValue);
+            return returnValue;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private string ValidForm()
+        {
+            string returnValue = string.Empty;
+            string textBoxValue = this.textBox1.Text.Trim();
+            int numRows;
+
+            if (textBoxValue.Length == 0)
+            {
+                returnValue = "You must enter a value.";
+            } else if (int.TryParse(textBoxValue, out numRows) == false )
+            {
+                returnValue = "You must enter an integer value.";
+            }
+
+            return returnValue;
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
